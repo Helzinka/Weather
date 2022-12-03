39f99a6eae445445035acfc93a57cda9a1b6998f
Helzinka <leveque.fra@gmail.com>
2022-12-03 22:19:27 +0100

    add scss support

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react"
+import "./App.scss"
 
 function App() {
-	return <div className="App"></div>
+	return (
+		<div className="App">
+			<div className="cards">
+				<p>ok</p>
+				<p>words</p>
+			</div>
+		</div>
+	)
 }
 
 export default App
